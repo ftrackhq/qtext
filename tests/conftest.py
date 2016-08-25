bf0144fc41ea3c17db7436c32c328c1b0a7b5d36
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-08-25 18:03:09 +0200

    prepare testbed for testing

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+import os
+
+
+@pytest.fixture
+def pyside():
+    os.environ["QT_PREFERRED_BINDING"] = "PySide"
+
+    def finalizer():
+        os.environment.pop('QT_PREFERRED_BINDING')
+
+
+@pytest.fixture
+def pyside2():
+    os.environ["QT_PREFERRED_BINDING"] = "PySide2"
+
+    def finalizer():
+        os.environment.pop('QT_PREFERRED_BINDING')
+
+
+@pytest.fixture
+def pyqt4():
+    os.environ["QT_PREFERRED_BINDING"] = "PyQt4"
+
+    def finalizer():
+        os.environment.pop('QT_PREFERRED_BINDING')
+
+
+@pytest.fixture
+def pyqt5():
+    os.environ["QT_PREFERRED_BINDING"] = "PyQt5"
+
+    def finalizer():
+        os.environment.pop('QT_PREFERRED_BINDING')
