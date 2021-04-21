e19cc6257fedfc32f132f39aae2d1fc2773840d8
Lluis Casals <lluis.casals@ftrack.com>
2021-04-21 11:17:24 +0200

    Revert "update sphinxs version and py2l docutils compatibility"
    This reverts commit 46fc35fcb481120aa99429f789183dfe581e88ea.

--- setup.py
+++ setup.py
@@ -39,8 +39,7 @@ setup(
     },
     setup_requires=[
         'Qt.py == 0.3.4',
-        'docutils<0.17',
-        'sphinx >= 1.8.5',
+        'sphinx >= 1.2.2, < 2',
         'sphinx_rtd_theme >= 0.1.6, < 2',
         'lowdown >= 0.1.0, < 1'
     ],
